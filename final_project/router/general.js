3f8be19a482830f939b2bc34bb33faccfe4f10bf
Victor <Victoranyadiegwu>
2026-04-15 07:17:29 -0400

    Task 11

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -51,18 +51,31 @@ public_users.get('/', async function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  const isbn = req.params.isbn;   
+// Task 11: Get book details based on ISBN using Async-Await
+public_users.get('/isbn/:isbn', async function (req, res) {
+    const isbn = req.params.isbn;
 
-    if (books[isbn]) {
+    try {
+        const fetchBookByIsbn = (isbn) => {
+            return new Promise((resolve, reject) => {
+                const book = books[isbn];
+                if (book) {
+                    resolve(book);
+                } else {
+                    reject({ status: 404, message: `Book with ISBN ${isbn} not found` });
+                }
+            });
+        };
+
+        const bookDetails = await fetchBookByIsbn(isbn);
+        
         res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(books[isbn], null, 4)); 
-    } else {
-        return res.status(404).json({ message: `Book with ISBN ${isbn} not found` });
+        return res.status(200).send(JSON.stringify(bookDetails, null, 4));
+
+    } catch (error) {
+        return res.status(error.status || 500).json({ message: error.message || "Internal Server Error" });
     }
-//   return res.status(300).json({message: "Yet to be implemented"});
- });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
